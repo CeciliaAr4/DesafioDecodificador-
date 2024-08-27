f804ccb30213fb4e6fdd61a08be9cea16fb607bd
Rafajoy <104836665+CeciliaAr4@users.noreply.github.com>
2024-08-26 21:36:35 -0300

    Feat: js finalizado

---
+++ scripts/interacao.js
@@ -0,0 +1,30 @@
+var textInput = document.querySelector("#input-text")
+var outInput = document.querySelector("#output")
+
+function criptografar(){
+    var texto = textInput.value; 
+
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
+    .replace(/o/g, "ober").replace(/u/g, "ufat");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-text">' + resultCripto +
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function descriptografar(){
+    var texto = textInput.value; 
+
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a")
+    .replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-text">' + resultDescripto +
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function copiar(){
+    var textCop = document.getElementById('input-text');
+
+    textCop.selecte();
+    document.execCommand('copy');
+    alert("Testo copiado"); 
+}
